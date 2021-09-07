b4518fe20b5be0075e92d7c4d8c663da83a673a9
minkyuu <minkyu023@gmail.com>
2021-09-08 02:57:04 +0900

    feat: 학점 산출 프로그램 구현

---
+++ grade_calculation_program/src/Score.java
@@ -0,0 +1,41 @@
+public class Score {
+
+    int studentId;
+    Subject subject;
+    int point;
+
+    public Score(int studentId, Subject subject, int point){
+        this.studentId = studentId;
+        this.subject = subject;
+        this.point = point;
+    }
+
+    public int getStudentId(){
+        return studentId;
+    }
+
+    public void setStudentId(int studentId){
+        this.studentId = studentId;
+    }
+
+    public Subject getSubject(){
+        return subject;
+    }
+
+    public void setSubject(Subject subject){
+        this.subject = subject;
+    }
+
+    public int getPoint(){
+        return point;
+    }
+
+    public void setPoint(){
+        this.point = point;
+    }
+
+    public String toString(){
+        return "학번:"+studentId+"," + subject.getSubjectName()+":"+point;
+    }
+
+}
---
+++ grade_calculation_program/src/evalutaion/GradeEvaluation.java
@@ -0,0 +1,5 @@
+package evalutaion;
+
+public interface GradeEvaluation {
+    public String getGrade(int point);
+}
